cf61b716bb5bfc55f3ee8848db4138a01b8ce56b
Mitchell Burton <mitch@mitchellburton.ca>
2021-12-02 20:56:49 -0800

    initial cdk commit

---
+++ test/terraria-server-aws.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as TerrariaServerAws from '../lib/terraria-server-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/terraria-server-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TerrariaServerAws.TerrariaServerAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
