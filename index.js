c2b428b998f99109b97c111f0d3de22fc4b939be
Mario Viens <marviens@gmail.com>
2021-12-02 23:32:08 -0800

    Add css lib, add better loading messages, make js work

--- index.js
+++ index.js
@@ -5,23 +5,22 @@ const getStatus = 'status'
 const currentStateId = 'current-state'
 const changeStateButtonId = 'change-state'
 
-// On load
-function ready(fn) {
-  if (document.readyState != 'loading'){
-    fn()
-  } else {
-    document.addEventListener('DOMContentLoaded', fn)
-  }
+// On load...
+if (document.readyState != 'loading'){
+  fn()
+} else {
+  document.addEventListener('DOMContentLoaded', fn)
 }
-// Do this
-fn = () => {
+
+// ...Do this
+function fn() {
   const currentState = document.getElementById(currentStateId)
   const changeStateButton = document.getElementById(changeStateButtonId)
 
   fetch(apiGatewayUrl + getStatus)
     .then(response => {
       const status = response.json() // TODO: get the body or whatever
-      const notStatus = status == "On" ? "Off" : "On" 
+      const notStatus = status == "On" ? "Off" : "On"
       // Make sure we're not replacing with garbage data...
       if (status) {
         currentState.textContent(`Status: ${status}`)
