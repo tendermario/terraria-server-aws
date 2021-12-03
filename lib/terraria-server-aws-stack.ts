a96c5cfc165548813c5ad625dc94d9b1087e9b3f
Mitchell Burton <mitch@mitchellburton.ca>
2021-12-02 23:06:19 -0800

    nearly working request auth lambda

--- lib/terraria-server-aws-stack.ts
+++ lib/terraria-server-aws-stack.ts
@@ -1,9 +1,16 @@
+require('dotenv').config()
+const crypto = require('crypto')
+import * as path from 'path'
+
+import * as apigateway from '@aws-cdk/aws-apigateway'
 import * as cdk from '@aws-cdk/core'
 import * as lambda from '@aws-cdk/aws-lambda'
 
 export class TerrariaServerStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
+
+    const lambdaDir = path.join(__dirname, 'lambdas')
 
     const startLambda = new lambda.Function(this, 'StartTerrariaServerLambda', {
       runtime: lambda.Runtime.NODEJS_14_X,
@@ -25,5 +32,33 @@ export class TerrariaServerStack extends cdk.Stack {
       code: lambda.Code.fromInline('TODO'),
       functionName: 'TerrariaServerStatusLambda',
     })
+
+    const authLambda = new lambda.Function(this, 'TerrariaServerAuthLambda', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'index.handler',
+      code: lambda.AssetCode.fromAsset(lambdaDir + '/auth-lambda'),
+      functionName: 'TerrariaServerAuthLambda',
+      environment: {
+        'PASSWORD': process.env.PASSWORD ?? crypto.randomUUID()
+      },
+    })
+
+    const api = new apigateway.RestApi(this, 'TerrariaServerApi')
+    const authorizer = new apigateway.RequestAuthorizer(this, 'TerrariaServerAuthorizer', {
+      handler: authLambda,
+      identitySources: [apigateway.IdentitySource.header('Authorization')]
+    })
+
+    api.root
+      .addResource('start')
+      .addMethod('POST', new apigateway.LambdaIntegration(startLambda), { authorizer })
+
+    api.root
+      .addResource('stop')
+      .addMethod('POST', new apigateway.LambdaIntegration(stopLambda), { authorizer })
+
+    api.root
+      .addResource('status')
+      .addMethod('GET', new apigateway.LambdaIntegration(statusLambda), { authorizer })
   }
 }
