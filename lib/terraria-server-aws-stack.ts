0310bd7f6da642e3e5a8c5d83ef8fedc0f68515e
Mitchell Burton <mitch@mitchellburton.ca>
2021-12-02 21:22:59 -0800

    dummy lambdas

--- lib/terraria-server-aws-stack.ts
+++ lib/terraria-server-aws-stack.ts
@@ -1,15 +1,29 @@
-import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as cdk from '@aws-cdk/core'
+import * as lambda from '@aws-cdk/aws-lambda'
 
-export class TerrariaServerAwsStack extends cdk.Stack {
+export class TerrariaServerStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
+    const startLambda = new lambda.Function(this, 'StartTerrariaServerLambda', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline('TODO'),
+      functionName: 'StartTerrariaServerLambda',
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TerrariaServerAwsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const stopLambda = new lambda.Function(this, 'StopTerrariaServerLambda', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline('TODO'),
+      functionName: 'StopTerrariaServerLambda',
+    })
+
+    const statusLambda = new lambda.Function(this, 'TerrariaServerStatusLambda', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline('TODO'),
+      functionName: 'TerrariaServerStatusLambda',
+    })
   }
 }
