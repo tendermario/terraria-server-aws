c8cbf3422525eaa316fbbc8004bc7ae33adc7446
Mario Viens <marviens@gmail.com>
2021-12-03 23:04:39 -0800

    Make status get req succeed and update page on load

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const apiGatewayUrl = 'ky331xbqw4.execute-api.us-west-2.amazonaws.com/prod/'
+const apiGatewayUrl = 'https://ky331xbqw4.execute-api.us-west-2.amazonaws.com/prod/'
 
 const getStatus = 'status'
 
@@ -13,22 +13,18 @@ if (document.readyState != 'loading'){
 }
 
 // ...Do this
-function fn() {
+async function fn() {
   const currentState = document.getElementById(currentStateId)
   const changeStateButton = document.getElementById(changeStateButtonId)
 
-  fetch(apiGatewayUrl + getStatus)
-    .then(response => {
-      const {result} = response.json().body
-      const status = result == "running" ? "On" : "Off"
-      const notStatus = result == "running" ? "Off" : "On"
-      // Make sure we're not replacing with garbage data...
-      if (result) {
-        currentState.textContent(`Status: ${status}`)
-        changeStateButton.textContent(`Turn: ${notStatus}`)
-      }
-    })
-    .catch(err => {
-      console.error(`Some error ${err}`)
-    })
+  const response = await fetch(apiGatewayUrl1 + getStatus)
+  const {result} = await response.json()
+  const status = result == "running" ? "On" : "Off"
+  const notStatus = result == "running" ? "Off" : "On"
+  // Make sure we're not replacing with garbage data...
+  if (result) {
+    currentState.textContent = `Status: ${status}`
+    changeStateButton.textContent = `Turn ${notStatus}`
+    document.title = `Terraria server - ${status}`
+  }
 }
