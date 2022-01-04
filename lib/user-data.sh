0e30792ad15f776db58b54a6d68b0cc4a693ccf1
Mitchell Burton <mitch@mitchellburton.ca>
2022-01-03 19:17:48 -0800

    add fix to startup command https://github.com/ryansheehan/terraria/issues/82

--- lib/user-data.sh
+++ lib/user-data.sh
@@ -6,4 +6,4 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
     sudo usermod -a -G docker ec2-user
 
     mkdir -p $HOME/terraria/world
-    sudo docker run -p 7777:7777 --rm -v $HOME/terraria/world:/root/.local/share/Terraria/Worlds ryshe/terraria:latest -world /root/.local/share/Terraria/Worlds/world1.wld -autocreate 3 --log-opt max-size=200m
+    sudo docker run -p 7777:7777 --rm -v $HOME/terraria/world:/root/.local/share/Terraria/Worlds ryshe/terraria:latest -world /root/.local/share/Terraria/Worlds/world1.wld -autocreate 3 --log-opt max-size=200m -disable-commands
