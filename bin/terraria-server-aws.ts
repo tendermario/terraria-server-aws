6be206365b2b1fd9e9f8b220ac1302e4cb5c5905
Mario Viens <marviens@gmail.com>
2022-01-09 15:12:57 -0800

    Add FlatEarth vs InnMates. FlatEarth is at the root path

--- bin/terraria-server-aws.ts
+++ bin/terraria-server-aws.ts
@@ -3,8 +3,9 @@ import 'source-map-support/register'
 import * as cdk from 'aws-cdk-lib'
 import { TerrariaServerStack } from '../lib/terraria-server-aws-stack'
 
-const app = new cdk.App();
-new TerrariaServerStack(app, 'TerrariaServerStack', {
+const app = new cdk.App()
+
+new TerrariaServerStack(app, 'FlatEarth', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -19,3 +20,5 @@ new TerrariaServerStack(app, 'TerrariaServerStack', {
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 })
+
+new TerrariaServerStack(app, 'InnMates')
