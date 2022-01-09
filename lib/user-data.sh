90445b83303068442e1a403188bd885b86901fce
Mario Viens <marviens@gmail.com>
2022-01-08 16:38:19 -0800

    Use ec2-user instead root for docker command

--- lib/user-data.sh
+++ lib/user-data.sh
@@ -5,15 +5,16 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 sudo yum update -y
 sudo amazon-linux-extras install docker
 sudo service docker start
-sudo usermod -a -G docker ec2-user
+sudo usermod -aG docker ec2-user
 
 # Add some startup files
 cat <<EOT >> /etc/systemd/system/docker_terraria.service
 [Unit]
 After=docker.service
-Description=Docker compose
+Description=Start terraria in docker
 
 [Service]
+User=ec2-user
 ExecStart=/home/ec2-user/start-terraria.sh
 
 [Install]
@@ -22,7 +23,7 @@ EOT
 
 cat <<EOT >> /home/ec2-user/start-terraria.sh
 #!/bin/bash
-sudo docker run --rm --name="terraria" -p 7777:7777 -v $HOME/terraria/world:/root/.local/share/Terraria/Worlds ryshe/terraria:latest -world /root/.local/share/Terraria/Worlds/world1.wld --log-opt max-size=200m -disable-commands
+docker run --rm --name="terraria" -p 7777:7777 -v $HOME/terraria/world:/root/.local/share/Terraria/Worlds ryshe/terraria:latest -world /root/.local/share/Terraria/Worlds/world1.wld --log-opt max-size=200m -disable-commands
 EOT
 
 sudo chmod +x /home/ec2-user/start-terraria.sh
@@ -33,4 +34,4 @@ sudo systemctl enable docker_terraria.service
 
 # Install and run Terraria, creating a large world by default
 mkdir -p $HOME/terraria/world
-sudo docker run --name="terraria" -p 7777:7777 --rm -v $HOME/terraria/world:/root/.local/share/Terraria/Worlds ryshe/terraria:latest -world /root/.local/share/Terraria/Worlds/world1.wld -autocreate 3 --log-opt max-size=200m -disable-commands
+docker run --rm --name="terraria" -p 7777:7777 -v $HOME/terraria/world:/root/.local/share/Terraria/Worlds ryshe/terraria:latest -world /root/.local/share/Terraria/Worlds/world1.wld -autocreate 3 --log-opt max-size=200m -disable-commands
