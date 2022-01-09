34bc93cd61d09e820a4ca8fe1fb820fc7bb1de07
Mario Viens <marviens@gmail.com>
2022-01-09 15:13:16 -0800

    Lint

--- bin/terraria-server-aws.ts
+++ bin/terraria-server-aws.ts
@@ -3,7 +3,7 @@ import 'source-map-support/register'
 import * as cdk from 'aws-cdk-lib'
 import { TerrariaServerStack } from '../lib/terraria-server-aws-stack'
 
-const app = new cdk.App();
+const app = new cdk.App()
 new TerrariaServerStack(app, 'TerrariaServerStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
