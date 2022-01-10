0bec8b2fd71169434e056a81e92f8ca55051ef99
Mario Viens <marviens@gmail.com>
2022-01-09 16:51:25 -0800

    Refactor and add steps for creating multiple servers

--- bin/terraria-server-aws.ts
+++ bin/terraria-server-aws.ts
@@ -1,21 +1,36 @@
 #!/usr/bin/env node
 import 'source-map-support/register'
+require('dotenv').config()
+const crypto = require('crypto')
+
 import * as cdk from 'aws-cdk-lib'
 import { TerrariaServerStack } from '../lib/terraria-server-aws-stack'
 
+
+const email = process.env.EMAIL ?? ""
+const UIpassword = process.env.UIPASSWORD ?? crypto.randomUUID()
+const worldFileName = 'world.wld'
+
+if (!email) {
+  throw 'Should have an email created in a .env file, see Readme'
+}
+
 const app = new cdk.App()
-new TerrariaServerStack(app, 'TerrariaServerStack', {
+new TerrariaServerStack(app, 'Default', {
+  UIpassword,
+  email,
+  // Update this if your world file in s3-files has a different filename.
+  // Otherwise, leave it alone since it is the name of the world filename
+  // that it will generate.
+  worldFileName,
+  s3Files: './s3-files/default',
+
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
+  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 })
--- lib/terraria-server-aws-stack.ts
+++ lib/terraria-server-aws-stack.ts
@@ -1,5 +1,4 @@
 require('dotenv').config()
-const crypto = require('crypto')
 import * as path from 'path'
 import { readFileSync, existsSync } from 'fs'
 
@@ -17,37 +16,48 @@ import * as s3 from 'aws-cdk-lib/aws-s3'
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment'
 
 const App = 'Terraria'
-const email = process.env.EMAIL ?? ""
-const UIpassword = process.env.UIPASSWORD ?? crypto.randomUUID()
-const worldName = 'world.wld' // Update this if your world file in s3-files has a different filename.
-                              // Otherwise, leave it alone since it is the name of the world filename
-                              // that it will generate.
 
-if (!email) {
-  throw 'Should have an email created in a .env file, see Readme'
+
+interface TerrariaServerStackProps extends cdk.StackProps {
+  // The name of the world file
+  worldFileName: string;
+  // The email for alarms to go to
+  email: string;
+  // The password set to turn on/off the server in the frontend UI
+  UIpassword: string;
+  // (Optional) The location of the world data, if you want to move a world onto the server 
+  s3Files?: string;
 }
 
 // We create this under a class just to link together resources with 'this'
 export class TerrariaServerStack extends cdk.Stack {
-  constructor(scope: any, id: string, props?: cdk.StackProps) {
+  constructor(scope: any, id: string, props: TerrariaServerStackProps) {
     super(scope, id, props)
-
+    const {
+      worldFileName,
+      email,
+      UIpassword,
+      s3Files,
+    } = props
+    
     // S3
     const bucket = new s3.Bucket(this, 'ServerFiles', {versioned: true})
-    const assetFiles = `./s3-files/${id.toLowerCase()}`
+    const assetFiles = s3Files || ""
 
-    if (existsSync(assetFiles)) {
-      new s3deploy.BucketDeployment(this, `${App}${id}DeployFiles`, {
-        sources: [s3deploy.Source.asset(assetFiles)],
-        destinationBucket: bucket,
-        prune: false, // This makes it not delete the files if they already exist in s3
-      });
+    if (assetFiles && !existsSync(assetFiles)) {
+      throw "Cannot locate the files to upload to s3"
     }
+
+    new s3deploy.BucketDeployment(this, `${App}DeployFiles-${id}`, {
+      sources: [s3deploy.Source.asset(assetFiles)],
+      destinationBucket: bucket,
+      prune: false, // This makes it not delete the files if they already exist in s3
+    });
 
     // UserData start-up commands for EC2 Instance
     const commands = readFileSync(path.join(__dirname, 'user-data.sh'), 'utf8')
     const commandsReplaced = commands.replace(new RegExp('s3BucketName', 'g'), bucket.bucketName)
-            .replace(new RegExp('worldName', 'g'), worldName)
+            .replace(new RegExp('worldFileName', 'g'), worldFileName)
 
     const userData = ec2.UserData.forLinux()
     userData.addCommands(commandsReplaced)
@@ -58,7 +68,7 @@ export class TerrariaServerStack extends cdk.Stack {
       enableDnsSupport: true,
     })
 
-    const securityGroup = new ec2.SecurityGroup(this, `${App}SecurityGroup`, {
+    const securityGroup = new ec2.SecurityGroup(this, `${App}SecurityGroup-${id}`, {
       vpc,
       description: `Access to server ports for ec2 instance`
     })
@@ -66,7 +76,7 @@ export class TerrariaServerStack extends cdk.Stack {
     securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.udp(7777), `Allow ${App} server connections`)
     securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'allow ssh access from the world')
 
-    const ec2Instance = new ec2.Instance(this, `Server`, {
+    const ec2Instance = new ec2.Instance(this, `${id}Server`, {
       vpc,
       vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.SMALL),
@@ -80,7 +90,7 @@ export class TerrariaServerStack extends cdk.Stack {
     })
 
     // ec2 access S3
-    ec2Instance.role?.attachInlinePolicy(new iam.Policy(this, `Access${App}${id}S3`, {
+    ec2Instance.role?.attachInlinePolicy(new iam.Policy(this, `${App}AccessS3-${id}`, {
       document: new iam.PolicyDocument({
         statements: [new iam.PolicyStatement({
           actions: ['s3:*'],
@@ -91,14 +101,14 @@ export class TerrariaServerStack extends cdk.Stack {
 
     const {instanceId} = ec2Instance
     // EC2 Instance has Elastic IP
-    const eip = new ec2.CfnEIP(this, `${App}Ip`, {instanceId: instanceId})
+    const eip = new ec2.CfnEIP(this, `${App}Ip-${id}`, {instanceId: instanceId})
     // List the IP in the output
-    new cdk.CfnOutput(this, `${App}IpAddress`, { value: eip.ref });
+    new cdk.CfnOutput(this, `${App}IpAddress-${id}`, { value: eip.ref });
 
     // Lambdas
     const lambdaDir = path.join(__dirname, 'lambdas')
 
-    const startLambda = new lambda.NodejsFunction(this, `Start${App}${id}ServerLambda`, {
+    const startLambda = new lambda.NodejsFunction(this, `${App}StartServerLambda-${id}`, {
       entry: path.join(lambdaDir, 'start-lambda.ts'),
       handler: 'handler',
       functionName: `Start${App}ServerLambda`,
@@ -106,7 +116,7 @@ export class TerrariaServerStack extends cdk.Stack {
         'INSTANCE_ID': instanceId,
       },
     })
-    startLambda.role?.attachInlinePolicy(new iam.Policy(this, `Start${App}${id}Ec2Policy`, {
+    startLambda.role?.attachInlinePolicy(new iam.Policy(this, `${App}StartEc2Policy-${id}`, {
       document: new iam.PolicyDocument({
         statements: [new iam.PolicyStatement({
           actions: ['ec2:StartInstances'],
@@ -115,7 +125,7 @@ export class TerrariaServerStack extends cdk.Stack {
       }),
     }))
 
-    const stopLambda = new lambda.NodejsFunction(this, `Stop${App}${id}ServerLambda`, {
+    const stopLambda = new lambda.NodejsFunction(this, `${App}StopServerLambda-${id}`, {
       entry: path.join(lambdaDir, 'stop-lambda.ts'),
       handler: 'handler',
       functionName: `Stop${App}ServerLambda`,
@@ -123,7 +133,7 @@ export class TerrariaServerStack extends cdk.Stack {
         'INSTANCE_ID': instanceId,
       },
     })
-    stopLambda.role?.attachInlinePolicy(new iam.Policy(this, `Stop${App}${id}Ec2Policy`, {
+    stopLambda.role?.attachInlinePolicy(new iam.Policy(this, `${App}StopEc2Policy-${id}`, {
       document: new iam.PolicyDocument({
         statements: [new iam.PolicyStatement({
           actions: ['ec2:StopInstances'],
@@ -132,7 +142,7 @@ export class TerrariaServerStack extends cdk.Stack {
       }),
     }))
 
-    const statusLambda = new lambda.NodejsFunction(this, `${App}${id}ServerStatusLambda`, {
+    const statusLambda = new lambda.NodejsFunction(this, `${App}ServerStatusLambda-${id}`, {
       entry: path.join(lambdaDir, 'status-lambda.ts'),
       handler: 'handler',
       functionName: `${App}ServerStatusLambda`,
@@ -140,7 +150,7 @@ export class TerrariaServerStack extends cdk.Stack {
         'INSTANCE_ID': instanceId,
       },
     })
-    statusLambda.role?.attachInlinePolicy(new iam.Policy(this, `${App}${id}Ec2StatusPolicy`, {
+    statusLambda.role?.attachInlinePolicy(new iam.Policy(this, `${App}Ec2StatusPolicy-${id}`, {
       document: new iam.PolicyDocument({
         statements: [new iam.PolicyStatement({
           actions: ['ec2:DescribeInstanceStatus'],
@@ -149,7 +159,7 @@ export class TerrariaServerStack extends cdk.Stack {
       }),
     }))
 
-    const authLambda = new lambda.NodejsFunction(this, `${App}${id}ServerAuthLambda`, {
+    const authLambda = new lambda.NodejsFunction(this, `${App}ServerAuthLambda-${id}`, {
       entry: path.join(lambdaDir, 'auth-lambda.ts'),
       handler: 'handler',
       functionName: `${App}ServerAuthLambda`,
@@ -159,14 +169,14 @@ export class TerrariaServerStack extends cdk.Stack {
     })
 
     // API Gateway
-    const api = new apigateway.RestApi(this, `${App}ServerApi`, {
+    const api = new apigateway.RestApi(this, `${App}ServerApi-${id}`, {
       defaultCorsPreflightOptions: {
         allowOrigins: apigateway.Cors.ALL_ORIGINS,
         allowMethods: apigateway.Cors.ALL_METHODS,
         allowHeaders: ['Authorization'],
       }
     })
-    const authorizer = new apigateway.RequestAuthorizer(this, `${App}${id}ServerAuthorizer`, {
+    const authorizer = new apigateway.RequestAuthorizer(this, `${App}ServerAuthorizer-${id}`, {
       handler: authLambda,
       identitySources: [apigateway.IdentitySource.header('Authorization')],
       resultsCacheTtl: Duration.seconds(0),
@@ -193,7 +203,7 @@ export class TerrariaServerStack extends cdk.Stack {
 
     new cloudwatch.Alarm(this, `${App} ec2 instance high CPU usage`, {
       metric: new cloudwatch.Metric({
-        namespace: 'TerrariaServerStack/Server',
+        namespace: `${App}ServerStack/Server-${id}`,
         metricName: 'CPUUtilization',
         statistic: 'avg',
         label: 'Average CPU usage %'
@@ -211,7 +221,7 @@ export class TerrariaServerStack extends cdk.Stack {
     // Create alarms on instance being on for 8 hours, 24 hours
     const alarmOnUptime = (hour: number) => new cloudwatch.Alarm(this, `${App} ec2 instance on for ${hour} hours`, {
       metric: new cloudwatch.Metric({
-        namespace: 'TerrariaServerStack/Server',
+        namespace: `${App}TerrariaServerStack-${id}`,
         metricName: 'CPUUtilization',
         statistic: 'avg',
         label: 'Average CPU usage %',
